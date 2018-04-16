9cd95036309a6d8d84fba3ae4e69e01b9b61975a
Fan <ya1995fatw@gmail.com>
2018-04-16 11:45:26 -0400

    predict

--- lib/predict_score.R
+++ lib/predict_score.R
@@ -2,18 +2,11 @@
 ############# Predict score #################
 #############################################
 
-# predict score
+# load package
+library(tidyr)
+library(reshape2)
 
-# define p_{a,j}
-pair.predict.score <- function(itemscore,
-                               user.weight,
-                               meanscore){
-  
-  # get normalizing factor k
-  k = 1 / sum(user.weight)
-  
-  return(sum(k * user.weight*(itemscore - meanscore),na.rm = TRUE))
-}
+# predict score
 
 predict.score <- function(train = movie_train,
                           test = movie_test,
@@ -22,59 +15,49 @@ predict.score <- function(train = movie_train,
                           run.threshold = FALSE,
                           run.bestn = FALSE){
   
-  # compute meanscore for each user
-  group.user.id <- as.character( names(
-    tapply(train$Score,train$User,mean)))
-  meanscore <- as.data.frame(list(mean.score = 
-                                    tapply(train$Score,train$User,mean),
-                                  user = group.user.id),
-                             stringsAsFactors = FALSE
-  )
+  #
+  if (max(train) > 2){
   
-  # initial prediction dataframe
-  pred <- data.frame(User = test$User,
-                     Movie = test$Movie,
-                     Score = rep(NA,length(test$Movie))
-  )
+    Movie_mat <- dcast(train,User~Movie)
+    movie_rowmean <- rowMeans(Movie_mat[,-1],na.rm = T)
+    Movie_mat[,-1] <- Movie_mat[,-1] - movie_rowmean
+    rownames(Movie_mat) <- Movie_mat$User
+    Movie_mat[is.na(Movie_mat)]=0
+    train_data <- Movie_mat[,-1]
   
-  cat("Begin computation, current progress is 0 %\n")
-  for(i in 1:nrow(pred)){
-    # print current progress
-    print(i)
-    if(i %% 2000 == 0) cat("current progress is",round(i * 100 / nrow(pred)),"%\n")
-    
-    this.neighbor <- select_neighbor(pred$User[i],
-                                     weight_mat = weight_mat,
-                                     run.threshold = run.threshold,
-                                     run.bestn = run.bestn)
-    col.ind <- match(this.neighbor,colnames(weight_mat))
-    user.weight <- weight_mat[which(colnames(weight_mat) == pred$User[i]), col.ind]
-    Meanscore <- meanscore$mean.score[match(sort(this.neighbor), meanscore$user)]
-    pair <- paste(train$User,train$Movie,sep = '-')
-    this.item.pair <- paste(this.neighbor,train$Movie[i],sep = '-')
-    itemscore <- train$Score[match(this.item.pair, pair)]
-    pred$Score[i] <- meanscore$mean.score[which(meanscore$user == pred$User[i])] +
-      pair.predict.score(itemscore = itemscore,
-                                        user.weight = user.weight,
-                                        meanscore = Meanscore)
-   if (i > 20) break
+    pred <- matrix(0,nrow = length(unique(test$User)), ncol = length(unique(test$Movie)) )
+    rownames(pred) <- sort(unique(test$User))
+    colnames(pred) <- sort(unique(test$Movie))
   }
-  return(pred)
-  
+  else{
+    pred <- matrix(0,nrow = nrow(test), ncol = ncol(test))
+    rownames(pred) <- rownames(test)
+    colnames(pred) <- colnames(test)
+  }
+    cat("Begin computation, current progress is 0 %\n")
+    for(i in 1:nrow(pred)){
+      # print current progress
+      print(i)
+      if(i %% 2000 == 0) cat("current progress is",round(i * 100 / nrow(pred)),"%\n")
+      
+      this.neighbor <- select_neighbor(rownames(pred)[i],
+                                       weight_mat = weight_mat,
+                                       run.threshold = run.threshold,
+                                       run.bestn = run.bestn)
+      col.ind <- match(this.neighbor,colnames(weight_mat))
+      user.weight <- weight_mat[i, col.ind]
+      k <- 1 / sum(user.weight)
+      item.ind <- match(colnames(pred),colnames(train_data))
+      itemscore <- train_data[col.ind,item.ind]
+      pred[i, ] <- movie_rowmean[i] + k * (as.numeric(user.weight) %*% as.matrix(itemscore))
+     if (i > 200) break
+    }
+    return(pred)
 }
-# 
-# weight_mat <- matrix(rnorm(5055*5055),5055,5055)
-# colnames(weight_mat) <- paste(unique(movie_train$User))
-# 
-# system.time(tmp <- pair.predict.score(user, item,
-#                                       data = movie_train,
-#                                       weight_mat,
-#                                       run.threshold = FALSE,
-#                                       run.bestn = FALSE))
-# 
-system.time(pred1 <- predict.score(train = movie_train,
-                                  test = movie_test,
-                                  weight_mat = weight_mat,
-                                  run.threshold = TRUE,
-                                  run.bestn = TRUE)
-)
+
+# system.time(pred1 <- predict.score(train = movie_train,
+#                                   test = movie_test,
+#                                   weight_mat = weights,
+#                                   run.threshold = TRUE,
+#                                   run.bestn = TRUE)
+# )
