9175aecf97f8b1f592fa987b9b95e07e4d354868
Iiivan <iiivanhpf@icloud.com>
2018-04-17 11:39:39 -0400

    select

--- lib/select_neighbor.R
+++ lib/select_neighbor.R
@@ -6,19 +6,20 @@ select_neighbor <- function(userid, weight_mat,
   if(is.null(colnames(weight_mat))) colnames(weight_mat) <- rownames(weight_mat)
   
   if (run.bestn == TRUE & run.threshold == FALSE){
-    vec <- weight_mat[,userid]
+    vec <- weight_mat[which(rownames(weight_mat) != userid),userid]
     name_v <- names(sort(vec,decreasing = T))[1:para$n]
   } else if (run.bestn == FALSE & run.threshold == TRUE){
-    vec <- weight_mat[,userid]
-    name_v <- names(vec > para$threshold)
+    vec <- weight_mat[which(rownames(weight_mat) != userid),userid]
+    name_v <- names(abs(vec) > para$threshold)
   } else if (run.bestn == TRUE & run.threshold == TRUE){
-    vec <- weight_mat[,userid]
-    vec <- vec > para$threshold
+    vec <- weight_mat[which(rownames(weight_mat) != userid),userid]
+    vec <- vec[abs(vec) > para$threshold]
     name_v <- names(sort(vec,decreasing = T))[1:para$n]
   }
   return(name_v)
 }
 
-#mat <- matrix(rnorm(100),nrow = 10)
+mat <- readRDS("ms_vec_train.RData")
+rownames(mat) <- colnames(mat)
 #colnames(mat) <- 1:10
-select_neighbor(5,mat,run.bestn = TRUE, run.threshold = TRUE,para = list(threshold = 30,n = 7))
+#select_neighbor("10010",mat,run.bestn = TRUE, run.threshold = TRUE,para = list(threshold = 0.3,n = 7))
