75706ef7dee39d483564d3bf537726302cfb2c18
Sitong Chen <sc4283@columbia.edu>
2018-04-17 11:28:51 -0400

    mae

--- lib/mae.R
+++ lib/mae.R
@@ -1,7 +1,7 @@
 ############################## MAE
 MAE_ <- function(pred_test, true_test){
-  # Input: predicted matrix & test set matrix (with user names in rownames).
-  # Output: return mean average error
+  # Input: predicted matrix & test set matrix
+  # Output: return mean absolute error
   mae <- mean(abs(pred_test - true_test), na.rm = T)
   return(mae)
 }
